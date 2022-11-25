992cec4e2af580758d66fa19e52fa2a453819d74
StaceyJo <stacey.l.joseph@gmail.com>
2022-11-24 19:56:42 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+// Set up:
+// 1. Copy/paste this table into a fresh file
+
+// Restful Routes
+// #	Action	    URL	        HTTP Verb	jsx view filename	mongoose method
+// 1	Index	    /logs/	    GET	        Index.jsx	        Log.find()
+// 2	Show
+// 3	New	        /logs/new	GET	        New.jsx	            none
+// 4	Create	    /logs/	    POST	    none	            Log.create(req.body)
+// 5	Edit
+// 6	Update
+// 7	Destroy
